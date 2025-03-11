7c687628ad3f4552532f84661828fbe392e670ce
Nbiba Bedis <bedisnbiba@gmail.com>
2025-03-11 13:13:05 +0100

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -110,7 +110,8 @@ impl Pty {
         dbg!("a pair");
 
         let mut cmd = CommandBuilder::new(command.cmd);
-        dbg!("az");
+        // https://github.com/wez/wezterm/issues/4205
+        cmd.env("PATH", std::env::var("PATH")?);
         cmd.args(&command.args);
         match command.cwd {
             Some(cwd) => cmd.cwd(cwd),
